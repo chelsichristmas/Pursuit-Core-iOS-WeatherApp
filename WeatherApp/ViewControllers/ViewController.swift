f18f4ada7f534b289155699abe2f908278627202
Chelsi Christmas <c.christmas95@gmail.com>
2020-02-02 20:46:16 -0500

    Added code that prevents user from entering anything but a number in text field delegate

--- WeatherApp/ViewControllers/ViewController.swift
+++ WeatherApp/ViewControllers/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    let acceptableCharacters = CharacterSet(charactersIn: "0123456789").inverted
+   // var unacceptableEntries: [Character] = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    var zipCodeEntry = ""
     @IBOutlet weak var cityForecastLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -21,6 +23,7 @@ class ViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
         textField.delegate = self
+        
         
     }
 
@@ -56,7 +59,34 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
 
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
+        guard let text = textField.text else {
+            fatalError("No Valid entry")
+        }
+        if text.count == 0 || text.count > 5 {
+            // TODO: Add some alert here that says please enter zip code and remove print statement
+            print(" Invalid Entry")
+        } else {
+            
+            zipCodeEntry = text
+            textField.resignFirstResponder()
+        }
+        return true
+        }
+        
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let components = string.components(separatedBy: acceptableCharacters)
+        let filtered = components.joined(separator: "")
+        if string == filtered  {
+            return true
+        } else {
+            //This line prevents the user from entering anything but a number
+            //TODO: Add alert that says please enter a valid zip code
+            print("Please enter a valid zipcode")
+            return false
+            
+        }
+    
     }
-}
 
+}
