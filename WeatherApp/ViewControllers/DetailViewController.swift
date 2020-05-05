fee03c40081d9963b0fa869d0324e5821ee84aec
Chelsi Christmas <c.christmas95@gmail.com>
2020-05-05 17:41:51 -0400

    - implemented dependency injection for data persistence - created function for when favorite button is pressed and added logic

--- WeatherApp/ViewControllers/DetailViewController.swift
+++ WeatherApp/ViewControllers/DetailViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import ImageKit
+import DataPersistence
 
 class DetailViewController: UIViewController {
     
@@ -15,26 +16,75 @@ class DetailViewController: UIViewController {
     
     public var weatherInfo: Details?
     public var cityName: String?
-    public var photoInfo: PhotoDetails?
+    public var photoInfo: PhotoDetails
+    private var dataPersistence: DataPersistence<PhotoDetails>
+    
+    
+    
+    init(dataPersistence: DataPersistence<PhotoDetails>, photoInfo: PhotoDetails) {
+        self.dataPersistence = dataPersistence
+        self.photoInfo = photoInfo
+        super.init(nibName: "ViewController", bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     
     override func loadView() {
         view = detailView
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
         updateUI()
+        detailView.favoriteButton.addTarget(self, action: #selector(favoriteButtonPressed(sender:)), for: .touchUpInside)
         
+        updateFavoriteBookmark(photoInfo: photoInfo)
+    }
+    
+    
+    @objc func favoriteButtonPressed(sender: UIButton) {
+        print("Button Pressed")
         
+        if dataPersistence.hasItemBeenSaved(photoInfo) {
+            
+            detailView.favoriteButton.setImage(UIImage(systemName: "bookmark"), for: .normal)
+            if let index = try? dataPersistence.loadItems().firstIndex(of: photoInfo) {
+                do {
+                    try dataPersistence.deleteItem(at: index)
+                } catch {
+                    print("error deleting photo")
+                }
+            }
+        } else {
+            do {
+                detailView.favoriteButton.setImage(UIImage(systemName: "bookmark.fill"), for: .normal)
+                try dataPersistence.createItem(photoInfo)
+                print("Photo Saved Successfully")
+            } catch {
+                print("error saving photo")
+            }
+        }
         
     }
+    
+    private func updateFavoriteBookmark(photoInfo: PhotoDetails) {
+        if dataPersistence.hasItemBeenSaved(photoInfo) {
+            detailView.favoriteButton.setImage(UIImage(systemName: "bookmark.fill"), for: .normal)
+        } else {
+            detailView.favoriteButton.setImage(UIImage(systemName: "bookmark"), for: .normal)
+        }
+    }
+    
+    
     
     
     func updateUI() {
         self.detailView.forecastLabel.text = "Weather Forecast for \(cityName!)"
         guard let weatherInfo = weatherInfo else { return }
-        self.detailView.imageView.getImage(with: photoInfo!.largeImageURL) { (result) in
+        self.detailView.imageView.getImage(with: photoInfo.largeImageURL) { (result) in
             switch result {
             case .failure(let appError):
                 print("Error getting image: \(appError)")
@@ -51,8 +101,7 @@ class DetailViewController: UIViewController {
         self.detailView.humidityLabel.text = "Humidity: \(weatherInfo.humidity)"
         self.detailView.windspeedLabel.text = "Windspeed: \(weatherInfo.windGust)"
         self.detailView.precipitationLabel.text = "Precipitation: \(weatherInfo.precipType ?? "No precipitation")"
-    
-        //high, low, sunrise, sunset, windspeed and precipitation
+        
         
     }
     
