5b444bcd141dc29d0ee74abd9439c55357130629
Chelsi Christmas <c.christmas95@gmail.com>
2020-05-05 17:43:45 -0400

    - added table view - conformed view controller to the delegate and data source of the table view - added loadPhotos function to retrieve favorites photos

--- WeatherApp/ViewControllers/FavoritesViewController.swift
+++ WeatherApp/ViewControllers/FavoritesViewController.swift
@@ -7,24 +7,72 @@
 //
 
 import UIKit
+import DataPersistence
 
 class FavoritesViewController: UIViewController {
 
+    private var photos = [PhotoDetails]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    private var dataPersistence = DataPersistence<PhotoDetails>(filename: "photos.plist")
+    @IBOutlet weak var tableView: UITableView!
+ 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        loadPhotos()
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    private func loadPhotos() {
+        
+        do {
+         try  photos = dataPersistence.loadItems()
+        } catch {
+            print("Unable to load photos")
+        }
+        
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+}
+
+extension FavoritesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos.count
     }
-    */
+    
+    
+   
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "photoCell", for: indexPath) as? PhotoCell else {
+            fatalError("Unable to deque phot cell")
+        }
+        
+        let photo = photos[indexPath.row]
+        cell.configureCell(photo: photo)
+        return cell
+    }
+}
 
+extension FavoritesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        240
+    }
 }
